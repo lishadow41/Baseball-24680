2d23f31842dc7954876bcf814d9f1a0ee29d30a7
HyunMin Cho <hm1041.cho@samsung.com>
2024-05-30 14:46:07 +0900

    [refactoring] page 37

--- Test_BaseBall/test_baseball.cpp
+++ Test_BaseBall/test_baseball.cpp
@@ -4,12 +4,23 @@
 #include <stdexcept>
 using namespace std;
 
-TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
+class BaseballFixture : public testing::Test
+{
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
-}
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			//PASS
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
