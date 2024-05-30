1ed6fef0097ac7d6714f9ec48fe710b0bca4d94c
HyunMin Cho <hm1041.cho@samsung.com>
2024-05-30 14:38:20 +0900

    [feature] page 30

--- Test_BaseBall/test_baseball.cpp
+++ Test_BaseBall/test_baseball.cpp
@@ -8,3 +8,8 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmatched) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
